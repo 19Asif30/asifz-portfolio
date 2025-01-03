734b5226c650b78af86bf6a507869e1fa39e07c1
Mohammed Asif Ali <mohammed.asif@altigreen.com>
2025-01-03 15:09:21 +0530

    Update resume with latest skills

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/AsifzNewResume.pdf";
+import resumePDF from "../../assets/Asifz_Resume_2025.pdf";
 import HeroImage from "../../assets/hero.png"
 
 const Hero = () => {
